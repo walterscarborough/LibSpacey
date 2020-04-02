dd0b2673fbf78cccea4508bf13ebd49850811e16
Walter Scarborough <walter.scarborough@gmail.com>
2020-04-02 00:34:17 -0400

    Fix bug in shared script message output function

--- scripts/shared/shared.sh
+++ scripts/shared/shared.sh
@@ -8,7 +8,8 @@ function shared.set_bash_error_handling() {
 }
 
 function shared.display_success_message() {
+  local -r message=$1
   local -r green_color_code='\033[1;32m'
   local -r default_color_code='\033[00m'
-  echo -e "${green_color_code}\\n${default_color_code} \\n"
+  echo -e "${green_color_code}\\n${message} ${default_color_code} \\n"
 }
