4eec9d521a1c4a728d9eb4c3c2c9446f8a1cec18
Walter Scarborough <walter.scarborough@gmail.com>
2020-04-05 01:25:00 -0400

    Add xcode lint, test, and validate scripts

---
+++ xcode/scripts/lint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function go_to_project_top_directory() {
+  local -r script_dir=$(dirname "${BASH_SOURCE[0]}")
+
+  cd "$script_dir/../.." || exit 1
+}
+
+function run_bash_linter() {
+  shellcheck -x xcode/scripts/*.sh
+}
+
+function main() {
+  go_to_project_top_directory
+  source ./scripts/shared/shared.sh || exit 1
+  shared.set_bash_error_handling
+
+  run_bash_linter
+
+  shared.display_success_message "Xcode linters ran successfully 🧹"
+}
+
+main
---
+++ xcode/scripts/validate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function go_to_project_top_directory() {
+  local -r script_dir=$(dirname "${BASH_SOURCE[0]}")
+
+  cd "$script_dir/../.." || exit 1
+}
+
+function main() {
+  go_to_project_top_directory
+  source ./scripts/shared/shared.sh || exit 1
+  shared.set_bash_error_handling
+
+  ./xcode/scripts/lint.sh
+  ./xcode/scripts/test.sh
+
+  shared.display_success_message "Xcode validation completed successfully 🧪"
+}
+
+main
